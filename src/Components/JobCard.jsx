07b0ea9e5a2bbe27d305e1016d66fcf67fb80e39
Chetainya <chetainyamanchanda@gmail.com>
2024-05-06 19:24:32 +0530

    creating a job listing page with a re-usable jobcard component and also implementing the infinite scroll logic

---
+++ src/Components/JobCard.jsx
@@ -0,0 +1,95 @@
+import React, { useState } from "react";
+import "./JobCard.css";
+// import Modal from "./Modal/Modal";
+
+const maxChars = 300;
+function JobCard({
+  jdLink,
+  minExp,
+  salaryCurrencyCode,
+  jobRole,
+  Company,
+  logoUrl,
+  location,
+  maxJdSalary,
+  minJdSalary,
+  jobDetailsFromCompany,
+}) {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  let ModalProps = {
+    jdLink,
+    minExp,
+    salaryCurrencyCode,
+    jobRole,
+    Company,
+    logoUrl,
+    location,
+    maxJdSalary,
+    minJdSalary,
+    jobDetailsFromCompany,
+  };
+
+  const toggleExpand = () => {
+    setModalIsOpen(true);
+  };
+
+  function closeModal() {
+    setModalIsOpen(false);
+  }
+
+  let estimatedSalary = null;
+  if (minJdSalary && maxJdSalary) {
+    estimatedSalary = `${minJdSalary}k - ${maxJdSalary}k `;
+  } else if (maxJdSalary) {
+    estimatedSalary = `${maxJdSalary}k`;
+  } else if (minJdSalary) {
+    estimatedSalary = `${minJdSalary}k`;
+  }
+
+  return (
+    <>
+      
+      {/* {modalIsOpen && <Modal onClose={closeModal} {...ModalProps} />} */}
+      <li className={`listItemContainer`}>
+        
+        <div className="logoContainer">
+          <div>
+            <img className="logo" src={logoUrl} alt="logo" />
+          </div>
+          <div className="basicInfo">
+            <h4>{Company}</h4>
+            <h4>{jobRole}</h4>
+            <h4>{location}</h4>
+          </div>
+        </div>
+        <p className="salary">
+          Estimated Salary : {estimatedSalary} {salaryCurrencyCode}
+        </p>
+        <h3>About Role</h3>
+        {/* <p>{jobDetailsFromCompany}</p> */}
+        <div className="jobDetails">
+        <p>{jobDetailsFromCompany.slice(0, maxChars)}</p>
+        {jobDetailsFromCompany.length > maxChars && (
+          <button onClick={toggleExpand} className="toggleButton">Show more</button>
+        )}
+        </div>
+        {minExp === null ? <div className="experience"><h4>No Minumum Experience</h4></div> : null}
+        {minExp && (
+          <div className="experience">
+            <h3>Minumum Experience</h3>
+            <p>{minExp} years</p>
+          </div>
+        )}
+        
+        
+        <div  className="apply">
+          <a href={jdLink} target="_blank">Easy Apply</a>
+        </div>
+      </li>
+     
+    </>
+  );
+}
+
+export default JobCard;
---
+++ src/Components/ListJobs.jsx
@@ -0,0 +1,93 @@
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { jobSliceActions } from '../Store/JobSlice';
+import JobCard from './JobCard';
+import './ListJobs.css'
+
+
+const limit = 10;
+let offset = 0;
+
+function ListJobs() {
+    let jobs = useSelector(state => state.jobs)
+    const dispatch = useDispatch();
+    const [loading  , setLoading] = useState(false);
+    
+    
+
+    async function getJobs(){
+
+      setLoading(true)
+      let response = await fetch("https://api.weekday.technology/adhoc/getSampleJdJSON" , {
+          method : 'POST',
+          headers  : {
+              "Content-Type" : "application/json"
+          },
+          body : JSON.stringify({
+              
+              "limit": limit,
+              "offset": offset
+             })
+             
+      })
+  
+      let data = await response.json();
+      offset = offset + limit
+      
+      dispatch(jobSliceActions.setJobs(data.jdList));
+      // dispatch(jobSliceActions.limit(offset));
+      setLoading(false);
+      
+  
+  }
+
+
+    if(!jobs.Jobs){
+      return <h3>No Jobs Found</h3>
+    }
+
+
+    const handleScroll = () => {
+     
+      if (
+        
+      // (scrollTop + clientHeight >= scrollHeight )
+      window.scrollY + window.innerHeight  >= document.documentElement.scrollHeight - 20
+      ) {
+        console.log('fetch')
+        getJobs(); // Fetch more jobs when user reaches the bottom of the page
+      }
+    };
+  
+    useEffect(() => {
+      
+      
+    getJobs();
+      window.addEventListener('scroll', handleScroll); // Add scroll event listener
+  
+      return () => {
+        window.removeEventListener('scroll', handleScroll);
+         // Remove event listener on component unmount
+         dispatch(jobSliceActions.clearJobs());
+      };
+    }, []);
+
+
+
+    
+
+  return (
+    <>
+    
+    <div className='ListContainer'>
+      <ul className='List'>
+      {jobs.Jobs.map((job) => <JobCard key={job.jdUid} jdLink={job.jdLink} salaryCurrencyCode={job.salaryCurrencyCode} minExp={job.minExp} jobRole={job.jobRole} jobDetailsFromCompany={job.jobDetailsFromCompany} maxJdSalary={job.maxJdSalary} minJdSalary={job.minJdSalary} location={job.location} Company={job.companyName} logoUrl={job.logoUrl}></JobCard>)}
+      </ul>
+      
+    </div>
+    {loading && <p>Loading</p>}
+    </>
+  )
+}
+
+export default ListJobs
