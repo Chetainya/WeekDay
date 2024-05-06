f22a2dc7abcf71eca02629125566c07b10cdab2c
Chetainya <chetainyamanchanda@gmail.com>
2024-05-06 17:50:09 +0530

    basic NavBar component and some changes to the styling of the app

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './NavBar.css'
+function NavBar() {
+  return (
+    <div className='NavBarContainer'>
+      <h1>Seach Jobs</h1>
+    </div>
+  )
+}
+
+export default NavBar
